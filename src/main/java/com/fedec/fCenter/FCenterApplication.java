a291aa0a8f4cc4605c986b355dac10c3735f325b
fedecmann <m.fedec11@gmail.com>
2021-01-21 12:59:18 +0100

    Initial commit

---
+++ src/main/java/com/fedec/fCenter/FCenterApplication.java
@@ -0,0 +1,13 @@
+package com.fedec.fCenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fedec/fCenter/FCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fedec.fCenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
